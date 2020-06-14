6d797119d960a4503d20d65ea5246f8ea7b1d3e2
沈杰 <shenjie@yunhx.net>
2020-06-14 17:43:23 +0800

    fix

--- zookeeper-server1/src/main/java/com/shenjies88/practice/zkserver1/ZookeeperServer1Application.java
+++ zookeeper-server1/src/main/java/com/shenjies88/practice/zkserver1/ZookeeperServer1Application.java
@@ -1,20 +1,7 @@
 package com.shenjies88.practice.zkserver1;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.zookeeper.serviceregistry.ServiceInstanceRegistration;
-import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
-import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author shenjies88
